15c6c1b35fa950ee25094b9c7fc5a021cf78e266
tenparsent <tenparsent@gmail.com>
2024-08-07 19:27:26 +0900

    palipali

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 let Phrase = require("tenparsent-palindrome");
 
-let string = prompt("パリンドロームをテストしたい文字列を入力してください:");
+function palindromeTester() {
+  let string = prompt("パリンドロームをテストしたい文字列を入力してください:");
+  let phrase = new Phrase(string);
 
-let phrase = new Phrase(string);
-
-if(phrase.palindrome()){
-    alert(`"${phrase.content}"はパリンドロームです`);
-} else {
+  if (phrase.palindrome()) {
+    alert(`"${phrase.content}"はパリンドロームです！`);
+  } else {
     alert(`"${phrase.content}"はパリンドロームではありません`)
+  }
 }
+
+let button = document.querySelector("#palindromeTester");
+button.addEventListener("click", function() {
+  palindromeTester();
+})
